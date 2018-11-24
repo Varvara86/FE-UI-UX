669c124561afa8e4abf2a27cc36ea2cfc6f163f6
Varvara <bariyan86@gmail.com>
2018-11-24 12:16:04 +0200

    add checkbox

--- js/script.js
+++ js/script.js
@@ -11,59 +11,8 @@ $(document).ready(function(){
 });
 
 
-$(document).on('ready', function() {
-    $(".vertical-center-4").slick({
-      dots: true,
-      vertical: true,
-      centerMode: true,
-      slidesToShow: 4,
-      slidesToScroll: 2
-    });
-    $(".vertical-center-3").slick({
-      dots: true,
-      vertical: true,
-      centerMode: true,
-      slidesToShow: 3,
-      slidesToScroll: 3
-    });
-    $(".vertical-center-2").slick({
-      dots: true,
-      vertical: true,
-      centerMode: true,
-      slidesToShow: 2,
-      slidesToScroll: 2
-    });
-    $(".vertical-center").slick({
-      dots: true,
-      vertical: true,
-      centerMode: true,
-    });
-    $(".vertical").slick({
-      dots: true,
-      vertical: true,
-      slidesToShow: 3,
-      slidesToScroll: 3
-    });
-    $(".regular").slick({
-      dots: true,
-      infinite: true,
-      slidesToShow: 3,
-      slidesToScroll: 3
-    });
-    $(".center").slick({
-      dots: true,
-      infinite: true,
-      centerMode: true,
-      slidesToShow: 5,
-      slidesToScroll: 3
-    });
-    $(".variable").slick({
-      dots: true,
-      infinite: true,
-      variableWidth: true
-    });
-    $(".lazy").slick({
-      lazyLoad: 'ondemand', // ondemand progressive anticipated
-      infinite: true
-    });
-  });
+new Image().src = "http://counter.yadro.ru/hit?r"+
+			escape(document.referrer)+((typeof(screen)=="undefined")?"":
+				";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+                screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+";"+Math.random();
+                
