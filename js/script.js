6f17b631a41e3c9bae90468f35ae8d40a6929b1b
Varvara <bariyan86@gmail.com>
2018-12-04 17:59:26 +0200

    add slider

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
-$(document).ready(function(){
-    $('.openwnd').click(function(){
-        $('.poup').fadeIn();
-    });
-    $('.poup .close').click(function(){
-        $('.poup').fadeOut();
-    });
-    $('.poup .submit').click(function(){
-        alert("Нажата кнопка ''Действие ")
-    });
-});
+// $(document).ready(function(){
+//     $('.openwnd').click(function(){
+//         $('.poup').fadeIn();
+//     });
+//     $('.poup .close').click(function(){
+//         $('.poup').fadeOut();
+//     });
+//     $('.poup .submit').click(function(){
+//         alert("Нажата кнопка ''Действие ")
+//     });
+// });
 
 
 new Image().src = "http://counter.yadro.ru/hit?r"+
 			escape(document.referrer)+((typeof(screen)=="undefined")?"":
 				";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
-                screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+";"+Math.random();
+				screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+";"+Math.random();
+	
                 
---
+++ js/slider.js
@@ -0,0 +1,9 @@
+$(document).on('ready', function() {
+    $(".js-slider").slick({
+      dots: true,
+      infinite: true,
+      centerMode: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+  });
